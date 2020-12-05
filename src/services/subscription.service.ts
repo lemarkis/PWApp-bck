7432d592b7c4b85e9ca4c496d2e758a7cdf199a2
Jean Lemarquis <jean.lemarquis@epitech.eu>
2020-12-05 04:16:09 +0100

    filter sub creation

--- src/services/subscription.service.ts
+++ src/services/subscription.service.ts
@@ -1,13 +1,17 @@
 import Subscription, { ISubscription, SubscriptionDocument } from '../models/subscription.model';
 
 class SubscriptionService {
-  public static async getSubscriptionByUserId(id: any): Promise<SubscriptionDocument[]> {
+  public static async getSubscriptionByUserId(id: string): Promise<SubscriptionDocument[]> {
     return Subscription.find({ user_id: id }).exec();
   }
 
   public static async createSubscription(subscription: ISubscription):
   Promise<SubscriptionDocument | null> {
-    return Subscription.create(subscription);
+    const exist = await Subscription.find({ user_id: subscription.user_id });
+    if (!exist) {
+      return Subscription.create(subscription);
+    }
+    return null;
   }
 }
 
